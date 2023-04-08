d5ba1ca74282c44640cd587ea7ee36c3802b86b2
Frank Duerr <frank.duerr@ipvs.uni-stuttgart.de>
2023-04-08 12:12:32 +0200

    Added stubs for XSK program.

---
+++ xdp/xdp-04-xsk/src/xdp-xsk-bpf.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include <linux/bpf.h>
+//#include <linux/if_ether.h>
+//#include <linux/types.h>
+
+#include <bpf/bpf_helpers.h>
+
+// This map is required by an XSK program to indicate, which RX queues should be redirected
+// to an XSK (socket of type AF_XDP bound by the application). 
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 64); // assume there are at maximum this number of RX queues for a network device
+	__type(key, sizeof(uint32_t)); 
+	__type(value, sizeof(uint32_t));
+} xsks_map SEC(".maps");
+
+
+SEC("xdp-xsk")
+int xdp_prog_main(struct xdp_md *ctx)
+{
+	int index = ctx->rx_queue_index;
+	
+	// Check whether an XSK (socket of type AF_XDP) has been bound to RX queue of the device
+	// from which the current packet has been received.
+	if (bpf_map_lookup_elem(&xsks_map, &index))
+		return bpf_redirect_map(&xsks_map, index, 0);
+
+	// No XSK bound for this RX queue -> pass on packet to network stack
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
---
+++ xdp/xdp-04-xsk/src/xdp-xsk-user.c
@@ -0,0 +1,12 @@
+#define EXIT_OK 0
+#define EXIT_FAIL_BPFLOAD 1
+#define EXIT_FAIL_FINDMAP 2
+#define EXIT_FAIL_FINDELEM 3
+#define EXIT_FAIL_USAGE 4
+#define EXIT_FAIL_DEVICE 5
+
+int main(int argc, char *argv[])
+{
+
+	return EXIT_OK;
+}
